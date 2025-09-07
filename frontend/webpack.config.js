05d49cf4a8454b5c95035bc5c89eafc8a4595cab
Caim0149 <renath31@gmail.com>
2025-09-07 20:13:28 +0900

    feat: webpack hot reload setup

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,44 +1,90 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js',
-    clean: true,
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "bundle.js",
+        clean: true,
+        publicPath: "/",
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[name][ext]",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new ReactRefreshWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    devServer: {
+        static: [
+            {
+                directory: path.join(__dirname, "public"),
+                publicPath: "/",
+            },
+        ],
+        compress: true,
+        port: 3000,
+        host: "0.0.0.0",
+        open: true,
+        hot: true,
+        liveReload: true,
+        watchFiles: {
+            paths: ["src/**/*", "public/**/*"],
+            options: {
+                usePolling: true,
+                interval: 300,
+                ignored: /node_modules/,
+            },
+        },
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
+            progress: true,
+            webSocketURL: "auto://0.0.0.0:0/ws",
+        },
+        devMiddleware: {
+            writeToDisk: false,
+        },
     },
-    compress: true,
-    port: 3000,
-    open: true,
-  },
+    watchOptions: {
+        poll: 1000,
+        ignored: /node_modules/,
+    },
 };
