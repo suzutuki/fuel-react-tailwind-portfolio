93ef58716eb618f28b31f8083b991c9c73abf0af
Caim0149 <renath31@gmail.com>
2025-09-19 22:38:24 +0900

    コメントを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from './components/Home';
-
+// Appコンポーネントを定義してHomeコンポーネントをレンダリング
 const App: React.FC = () => {
     return <Home />;
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './globals.css';
 import App from './App';
-
+// ここではReact 18の新しいルートAPIを使用しています
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
+// APPコンポーネントをレンダリング
 root.render(
   <React.StrictMode>
     <App />
